b596c0854647ca282b20e9433ca1063d1814f8e0
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-01-21 23:13:03 +0530

    Added

---
+++ script.js
@@ -0,0 +1,23 @@
+// const searchBarText = document.querySelector(".search_bar_text");
+// const searchTextBox = document.querySelector(".search_bar_textbox");
+// const inputElement = searchBarText.querySelector(".search_bar_inner_location"); 
+// const svgElement = searchBarText.querySelector("span svg");  
+
+// const toggleSearchBoxStyle = () => {
+//     if (document.activeElement === inputElement || document.activeElement === svgElement) {
+//         searchTextBox.style.cssText = `
+//             opacity: 1;
+//             transform: scale(1);
+//         `;
+//     } else {
+//         searchTextBox.style.cssText = `
+//             opacity: 0;
+//             transform: scale(0.8);
+//         `;
+//     }
+// };
+
+// searchBarText.addEventListener("click", toggleSearchBoxStyle);
+// inputElement.addEventListener("focus", toggleSearchBoxStyle);
+// inputElement.addEventListener("blur", toggleSearchBoxStyle);
+// svgElement.addEventListener("click", toggleSearchBoxStyle);
